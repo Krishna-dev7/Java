de2cf96faed40bd8d3a6c708900cea7129eec99c
Flash0025 <nahakdaktar084@gmail.com>
2023-10-26 22:54:04 +0530

    Added basic folder

---
+++ Basic/hello.java
@@ -0,0 +1,6 @@
+package Basic;
+public class hello {
+    public static void main(String args[]) {
+       System.out.println("I am enjoying the step lectures, and we all appreciate it.");
+    }
+}   
