7de737b8ad93ebfa2f540fb5c468608235f542fd
Krishna <suprabha205@gmail.com>
2023-11-05 23:13:08 +0530

    delete Basic

--- Basic/hello.java
+++
@@ -1,6 +0,0 @@
-package Basic;
-public class hello {
-    public static void main(String args[]) {
-       System.out.println("I am enjoying the step lectures, and we all appreciate it.");
-    }
-}   
